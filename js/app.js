fd65758b5a46cc7e64e8096997c95b8bd2207705
FrankGrillo1991 <fsg12491@aol.com>
2023-07-18 12:50:17 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,1 @@
-const alertBanner = document.getElementById("alert");
-
-alertBanner.innerHTML =
-`<div class="alert-banner>
-<p><strong>Alert:</strong>You have <strong>6</strong> overdue tasks to complete</p>
-<p class="alert-banner-close">x</p>
-</div>
-`
+const alertBanner = <p><strong>Alert:</strong>You have <strong>6</strong> overdue tasks to complete</p>;
