dc3370284a34a755b5cd64ffb636a52040021a10
FrankGrillo1991 <fsg12491@aol.com>
2023-07-18 15:00:02 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const alertBanner = document.getElementById("alert");
-const trafficChart = document.getElementById("daily");
+const trafficCanvas = document.getElementById("daily-chart");
 
 alertBanner.innerHTML = `<div class="alert-banner"><p><strong>Alert:</strong>You have unread messages</p>
 <p class="alert-banner-close">x</p>
